5779f2bc3cef1ace7f2feacbc43da86653e89a32
AkaDNT <nhatthong346@gmail.com>
2026-04-03 18:43:03 +0700

    fix: api base url

--- apps/web/src/shared/config/env.ts
+++ apps/web/src/shared/config/env.ts
@@ -1,3 +1,11 @@
+const apiBaseUrl =
+  process.env.NEXT_PUBLIC_API_BASE_URL ||
+  (process.env.NODE_ENV === "development" ? "http://localhost:4000" : "");
+
+if (!apiBaseUrl) {
+  throw new Error("Missing NEXT_PUBLIC_API_BASE_URL");
+}
+
 export const ENV = {
-  apiBaseUrl: process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:4000",
+  apiBaseUrl,
 } as const;
